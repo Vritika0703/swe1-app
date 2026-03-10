7e8db2009f9f25bce0a7bf1ef7d9223143e1f4d7
Vritika Srivastava <vri24@Vritikas-MacBook-Air.local>
2026-03-09 22:17:25 -0400

    trigger coverage build

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase  # noqa: F401
+from django.urls import reverse
 
 # Create your tests here.
+
+class PollTests(TestCase):
+    def test_polls_page(self):
+        response = self.client.get(reverse("polls:index"))
+        self.assertEqual(response.status_code, 200)
